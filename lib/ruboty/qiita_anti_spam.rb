31dd5b0ea1861471038eb999c56aac07e7067376
Tomoki Aonuma <uasi@uasi.jp>
2015-11-19 17:18:41 +0900

    Initial commit

---
+++ lib/ruboty/qiita_anti_spam.rb
@@ -0,0 +1,7 @@
+require "ruboty/qiita_anti_spam/version"
+
+module Ruboty
+  module QiitaAntiSpam
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/qiita_anti_spam/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module QiitaAntiSpam
+    VERSION = "0.1.0"
+  end
+end
